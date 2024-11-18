b3f7ac15bbf69b7d38a37c8539e52893e939dd21
Partha Sarathi Giri <parthasarathikems@outlook.com>
2024-11-18 18:02:27 +0530

    Added some answers

---
+++ Assignemnt_functions.py
@@ -0,0 +1,44 @@
+# 1. What is the difference between a function and a method in Python?
+'''
+Function--
+A function is defined as structured , reusable code block utilized for carrying out a specific task.
+Fucntion facilitate the creation of modular codee simplifying , maintainence and reusability syntax.
+It can be use anywhere
+
+
+Methods--
+A method is a function that is defined within a class and is associated with an object. 
+Methods usually operate on data contained within the object.
+Methods are always defined within a class.
+It is used for specific tasks.
+'''
+
+# 2. Explain the concept of function arguments and parameters in Python.
+'''
+Arguments --
+Arguments are the actual value assignmed by the user for some varibale
+They provide data to the variable that perrfomrs cerrtain tasks
+
+
+Parameters--
+ Paramaeters are the variables listed inside the parentesis in the funxtion defination.
+ They act as placeholders for the values that will be pass into the function when the task is calledd t be performed
+ '''
+
+# 3. What are the different ways to define and call a function in Python?
+'''
+There are different methods to call a function-
+a.Basic function definations
+    def func():
+        print("Pw-Skills")
+
+b.Functions with paraameters
+    def greetins(name):
+        print("welcomw to off Mr.", name)
+        
+c.Function with the return values
+    def add(a=0,b=0):
+        return(a+b)
+        
+d.Anonymus function -- Lambda function
+    
